da287d6b1cddf0aafa66817b1d5b79bdf9e66415
Your Name <you@example.com>
2023-03-28 08:42:31 -0400

    add authication nav bar

---
+++ src/composables/auth/AuthNavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+      <router-link :to="{ name: 'LoginForm'}">Login</router-link> 
+    </nav>
+    <!-- <router-link :to="{ name: 'SignUp', params: { id: job.id}}"> -->
+</template>
+<script>
+
+
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
