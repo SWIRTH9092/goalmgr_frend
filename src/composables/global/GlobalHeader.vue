ae1b097340ea4e618f1f6e5c976b9dfaa0fa9a92
Your Name <you@example.com>
2023-03-28 08:12:36 -0400

    added global website header

---
+++ src/composables/global/GlobalHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="headerbox">
+            <br>
+            <h1>Goal Manager</h1> 
+            <br>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+    .headerbox {
+        background-color: var(--color3);
+        text-align: center; 
+    }
+
+    h1 {
+        text-align:center;
+        color: var(--color10);
+    }   
+</style>
