73c0c71b3a492791f7d5c99cd332d7654f9e4000
Your Name <you@example.com>
2023-03-28 09:20:34 -0400

    add SignUpView to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
+import SignupView from '../views/auth/SignUpView.vue'
 
 const routes = [
   {
@@ -9,15 +10,24 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
+    path: '/login',
     name: 'LoginView',
     component: LoginView
+
+  },
+  {
+    path: '/signup',
+    name: 'SignupView',
+    component: SignupView
+
+  }
+]
+
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
