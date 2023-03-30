17696b13d48410653afc8b18e1021136b17e32d3
Your Name <you@example.com>
2023-03-30 08:38:29 -0400

    move async function for data get data from methods to mounted

--- src/views/goallist/GoalListView.vue
+++ src/views/goallist/GoalListView.vue
@@ -1,42 +1,38 @@
 <template >
     <div>
-        <h1>Goal Index Page</h1>
+        <h1>Goal List Page</h1>
+
         <form @submit.prevent="handleCreateGoal">
 
             <label>Name:</label>
             <input type="text" 
-                v-model="createGoal.g_Name" 
+                v-model="createGoalList.gl_Name" 
                 placeholder="goal name" 
                 required 
             >
-            <label>Status:</label>
-            <input type="text" 
-                v-model="createGoal.g_Status" 
-                placeholder="status" 
-                required 
-            >   
-            <!-- <label for="status">Choose a Status:</label>
-                <select name="status" id="status" v-model="createGoal.g_Status">
+
+            <label for="status">Choose a Status:</label>
+                <select name="status" id="status" v-model="createGoalList.gl_Stat">
                     <option value="Not Started">Not started</option>
                     <option value="In Progress">In Progress</option>
                     <option value="Completed">Completed</option>
                 </select>
-            <br> -->
+            <br>
 
             <label>Description:</label>
             <input type="text" 
-                v-model="createGoal.g_Description" 
+                v-model="createGoalList.gl_Description" 
                 placeholder="description" 
                 >
 
             <label>Start Date:</label>        
             <input type="date" 
-                v-model="createGoal.g_StartDate" 
+                v-model="createGoalList.gl_StartDate" 
             >
 
             <label>End Date:</label>        
             <input type="date" 
-                v-model="createGoal.g_EndDate" 
+                v-model="createGoalList.gl_EndDate" 
             >
                 
             <div class="submit">
@@ -49,7 +45,6 @@
 <script>
 
 import axios from "axios";
-
 export default {
     name: "GoalView",
     data() {
@@ -57,51 +52,77 @@ export default {
             u_RootKey: '',
             isLoggedIn: '',
             createError: '',
-            createGoal: {
-                g_U_RootKey: '',
-                g_Name: '',
-                g_Status: '',
-                g_Description: '',
-                g_SortOrder: '',
-                g_StartDate: '',
-                g_EndDate: '',
+            currentDate: '',
+            createGoalList: {
+                gl_URootKey: '',
+                gl_Rootkey: '',
+                gl_Name: '',
+                gl_Stat: '',
+                gl_Description: '',
+                gl_StartDate: '',
+                gl_EndDate: '',
+                gl_SortOrder: 99,
             },
+            goallist: []
          }
     },
   
-    mounted() {
+    async mounted() {
         // get data from Local Storage
         this.u_RootKey = localStorage.getItem('u_RootKey')
         this.isLoggedIn = localStorage.getItem('isLoggedIn')
 
+        //get current date for 
+        const currentDate = new Date();
+            let currentDay = ''
+            let currentMonth = ''
+            if (currentDate.getDate() < 10) {
+                currentDay = '0' + currentDate.getDate()
+            } else {
+                currentDay = currentDate.getDate()
+            } 
+            if ((currentDate.getMonth()+1) < 10) {
+                currentMonth = '0' + (currentDate.getMonth()+1)
+            } else {
+                currentMonth = (currentDate.getMonth()+1)
+            } 
 
-        // call the function to read the data
-        // this.getGoalData(this.u_RootKey)
+            const currentDateFormatted = 
+            currentDate.getFullYear() + '-' +
+            currentMonth + '-' +
+            currentDay
+
+            this.currentDate = currentDateFormatted
+            this.createGoalList.gl_StartDate = currentDateFormatted
+            this.createGoalList.gl_EndDate = currentDateFormatted
+
+                    //get data for list
+            const url = process.env.VUE_APP_ROOT_API + '/goallist/index/' +     this.u_RootKey
+
+            await axios
+            .get(url)
+            .then((response) => {
+                this.goallist = response;
+                console.log(this.goallist)
+                console.log(response);
+            })
+                .catch((error) => {
+                    this.loginError = `Getting Data Unsuccessful - ${error}`;
+            });
     },
     methods: {
-        // async getGoalData(u_RootKey) {
-        //     console.log("root key", u_RootKey)
-        //     console.log("process.env",process.env.VUE_APP_ROOT_API)
-        //     const url = process.env.VUE_APP_ROOT_API + '/goal'
-        //     console.log(url)
-        //     await axios
-        //     .get(url)
-        //     .then((response) => {
-        //         console.log(response);
-        //     })
-        //         .catch((error) => {
-        //             this.loginError = `Getting Data Unsuccessful - ${error}`;
-        //     });
-        // },
+
         async handleCreateGoal(e) { 
             e.preventDefault();
+
+            //  update create record with the userid root key: u_rootkey 
+            //     from local storage
+            this.createGoalList.gl_URootKey = localStorage.getItem('u_RootKey')
+            console.log(this.createGoalList)
+
+            const url = process.env.VUE_APP_ROOT_API + '/goallist/create'
             await axios
-                .post("https://sw-goalmgr-bkend.onrender.com/goal/create",
-                {
-                    g_U_RootKey: "DT2023325183944-$2a$10$nEwYPyPJC1QNTq3qh.E11uaD.lHb7hzqdafs0hx60pLw35UH8ETlO",
-                    g_name: "shine shoes",
-                    g_status: "completed"
-                })
+                .post(url, this.createGoalList)
                 .then((response) => {
                     console.log("created Goal", response)
                     // router.push({ name: "GoalView" });
