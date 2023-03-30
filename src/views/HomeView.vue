25704f5c7589445151e32450e11be406a77e43bf
Your Name <you@example.com>
2023-03-30 13:29:17 -0400

    style homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,21 @@
     <div>
         <br>
         <br>
-        <h2>Dream Big!  Take your Vision and hit your Target!</h2>
+        <h2 class="pagecolor">Dream Big!  Take your Vision and hit your Target!</h2>
         <br>
         <div class="imagecontainer">
             <img src="https://i.imgur.com/P9U0I0c.jpg" alt="arrows hitting target" >
         </div>
-        <h2>Please Sign or Login</h2>
         <br>
-        <router-link :to="{ name: 'LoginView'}"><button>Login</button></router-link>   
-        <router-link :to="{ name: 'SignUpView'}"><button>Signup</button></router-link>       
+        <h2 class="pagecolor">Please Sign or Login</h2>
+        <router-link class="homelink" 
+            :to="{ name: 'LoginView'}">
+            <button class="homebutton">Login</button>
+        </router-link>   
+        <router-link  class="homelink" 
+            :to="{ name: 'SignUpView'}">
+            <button class="homebutton">Signup</button>
+        </router-link>       
     </div>
   </div>
 </template>
@@ -41,5 +47,18 @@ data () {
     img {
         object-fit: contain;
         border-radius: 15px;
-    }  
+    } 
+    
+    .homelink {
+        font-weight: bold;
+        padding: 10px;
+        color: var(--color8);
+    }
+    .homebutton {
+        background: var(--color8);
+        font-size: 1.5rem;
+    }
+    .pagecolor {
+        color: var(--color8);
+    }
 </style>
