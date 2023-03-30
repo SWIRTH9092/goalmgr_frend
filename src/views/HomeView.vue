4f9aa8f644a6ca00fdafb2638831643e36c566db
Your Name <you@example.com>
2023-03-30 13:07:13 -0400

    move app.vue styling to global.css file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
     <div>
+        <br>
+        <br>
         <h2>Dream Big!  Take your Vision and hit your Target!</h2>
+        <br>
         <div class="imagecontainer">
             <img src="https://i.imgur.com/P9U0I0c.jpg" alt="arrows hitting target" >
         </div>
         <h2>Please Sign or Login</h2>
+        <br>
         <router-link :to="{ name: 'LoginView'}"><button>Login</button></router-link>   
         <router-link :to="{ name: 'SignUpView'}"><button>Signup</button></router-link>       
     </div>
